2622b62d405cd6c6bb31424984dd984a75f6199d
ValenLopezVega <valen2004vega@gmail.com>
2025-10-27 15:20:17 -0500

    Delete wrong files and clean repository

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from models import db, Note
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
@@ -16,15 +17,13 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "default_secret_key")
 # Inicializamos la DB con Flask
 db.init_app(app)
 
+# Inicializamos Flask-Migrate
+migrate = Migrate(app, db)
+
 # Ruta de prueba
 @app.route("/")
 def home():
     return jsonify({"message": "Backend funcionando 🚀"})
-
-# Crear la base de datos antes del primer run
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
